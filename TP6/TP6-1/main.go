8914e0895a9fbd1e1da8e23833dcd5a64fd909d7
NahuelAlloaCasale <nahuelalloacasale@gmail.com>
2025-08-09 17:33:44 -0300

    .

---
+++ TP6/TP6-1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a      float64
+		b      float64
+		accion int = 1
+	)
+	fmt.Print("Ingrese el primer numero:")
+	fmt.Scanln(&a)
+	fmt.Print("Ingrese el segundo numero:")
+	fmt.Scanln(&b)
+	for accion != 0 {
+		fmt.Println("¿Que operacion desea realizar con dichos numeros?")
+		fmt.Println("1: Suma")
+		fmt.Println("2: Resta")
+		fmt.Println("3: Multiplicar")
+		fmt.Println("4: Dividir")
+		fmt.Println("0: Salir")
+		fmt.Scanln(&accion)
+		switch accion {
+		case 1:
+			fmt.Println("eligio SUMA")
+			resultado := suma(a, b)
+			fmt.Println("La suma de los numeros es", resultado)
+		case 2:
+			fmt.Println("eligio RESTA")
+			resultado := resta(a, b)
+			fmt.Println("La resta de los numeros es", resultado)
+		case 3:
+			fmt.Println("eligio MULTIPLICACION")
+			resultado := multiplicacion(a, b)
+			fmt.Println("El producto de los numeros es", resultado)
+		case 4:
+			fmt.Println("eligio DIVISION")
+			resultado := division(a, b)
+			fmt.Println("La division de los numeros es", resultado)
+		}
+	}
+	fmt.Print("Eligio SALIR")
+}
+func suma(a, b float64) (resultado float64) {
+	return a + b
+}
+func resta(a, b float64) (resultado float64) {
+	return a - b
+}
+func multiplicacion(a, b float64) (resultado float64) {
+	return a * b
+}
+func division(a, b float64) (resultado float64) {
+	return a / b
+}
